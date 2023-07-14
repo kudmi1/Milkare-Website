99e321b7eff98a9ab6da34525e44b100fd84412d
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-07-14 17:37:49 +0300

    fix header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState, useLayoutEffect, useRef } from "react"
 import HamburgerMenu from "./HamburgerMenu"
 
-export default function Header({ section, setSection, headerHeight, fontSize }) {
+export default function Header({
+	section,
+	setSection,
+	headerHeight,
+	fontSize,
+}) {
 	const [showHamburger, setShowHamburger] = useState(false)
 	const [position, setPosition] = useState(() => {
 		// Retrieve the stored position from localStorage
@@ -37,125 +42,124 @@ export default function Header({ section, setSection, headerHeight, fontSize }) 
 
 	return (
 		<div>
-
-		<header
-			className={`header fixed z-50 flex md:flex-row flex-col h-24 w-full justify-center 
-			 bg-gradient-to-tr from-[#212121d2] to-[#212121] opacity-100 backdrop-blur-[10px] transition-all duration-700 ${headerHeight}
+			<header
+				className={`header fixed z-50 flex h-24 w-full flex-col justify-center bg-gradient-to-tr 
+			 from-[#212121d2] to-[#212121] opacity-100 backdrop-blur-[10px] transition-all duration-700 md:flex-row ${headerHeight}
 			 `}
-		>
-			<div className="header-standart grid h-full w-full max-w-7xl grid-cols-2 items-center justify-between bg-transparent px-0 sm:px-6 md:grid-cols-3 xl:px-0 ">
-				<div className="mx-6 sm:mx-0 md:justify-center ">
-					<a
-						href="#"
-						className={`header-top effect-shine text-3xl transition-all duration-500 ${fontSize}`}
-					>
-						milkare
-					</a>
-				</div>
-				<nav className="mynav  black sticky bottom-0 left-0 z-[100] hidden md:flex h-full items-center justify-center">
-					<div
-						className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
-					></div>
-					<div
-						className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
-					></div>
-					<div
-						className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
-					></div>
-					<a
-						href="#gallery"
-						className="gallery-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
-						onClick={() => handleClick(0, "gallery")}
-					>
-						Gallery
-					</a>
-					<a
-						href="#price"
-						className="price-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
-						onClick={() => handleClick(1, "price")}
-					>
-						Price
-					</a>
-					<a
-						href="#info"
-						className="info-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
-						onClick={() => handleClick(2, "info")}
-					>
-						Info
-					</a>
-				</nav>
-
-				<div className="language-block hidden items-center justify-end md:flex">
-					<button className="language-item effect-shine language-eng mr-8 text-xl transition-all duration-500">
-						Eng
-					</button>
-					<button className="language-item effect-shine language-rus text-xl transition-all duration-500">
-						Rus
-					</button>
-				</div>
-				{showHamburger ? null : (
-					<div className="flex h-full items-center justify-end px-6 sm:px-0 md:hidden">
-						<button
-							onClick={toggleHamburger}
-							className="hamburger-btn effect-shine"
+			>
+				<div className="header-standart relative grid h-full w-full max-w-7xl grid-cols-2 items-center justify-between bg-transparent px-0 sm:px-6 md:grid-cols-3 xl:px-0 ">
+					<div className="mx-6 sm:mx-0 md:justify-center ">
+						<a
+							href="#"
+							className={`header-top effect-shine text-3xl transition-all duration-500 ${fontSize}`}
 						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth="1.5"
-								stroke="currentColor"
-								className="h-8 w-8"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-								/>
-							</svg>
+							milkare
+						</a>
+					</div>
+					<nav className="mynav black sticky bottom-0 left-0 z-[100] hidden h-full items-center justify-center md:flex">
+						<div
+							className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
+						></div>
+						<div
+							className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
+						></div>
+						<div
+							className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
+						></div>
+						<a
+							href="#gallery"
+							className="gallery-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
+							onClick={() => handleClick(0, "gallery")}
+						>
+							Gallery
+						</a>
+						<a
+							href="#price"
+							className="price-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
+							onClick={() => handleClick(1, "price")}
+						>
+							Price
+						</a>
+						<a
+							href="#info"
+							className="info-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
+							onClick={() => handleClick(2, "info")}
+						>
+							Info
+						</a>
+					</nav>
+
+					<div className="language-block hidden items-center justify-end md:flex">
+						<button className="language-item effect-shine language-eng mr-8 text-xl transition-all duration-500">
+							Eng
+						</button>
+						<button className="language-item effect-shine language-rus text-xl transition-all duration-500">
+							Rus
 						</button>
 					</div>
-				)}
-				<HamburgerMenu
-					hamburgerState={showHamburger}
-					setHamburgerState={setShowHamburger}
-				/>
-			</div>
-			<nav className="mynav black sticky bottom-0 left-0 z-[100] md:hidden flex h-full items-center justify-center">
-					<div
-						className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
-					></div>
-					<div
-						className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
-					></div>
-					<div
-						className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
-					></div>
-					<a
-						href="#gallery"
-						className="gallery-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
-						onClick={() => handleClick(0, "gallery")}
-					>
-						Gallery
-					</a>
-					<a
-						href="#price"
-						className="price-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
-						onClick={() => handleClick(1, "price")}
-					>
-						Price
-					</a>
-					<a
-						href="#info"
-						className="info-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
-						onClick={() => handleClick(2, "info")}
-					>
-						Info
-					</a>
-				</nav>
-		</header>
-		
+					{showHamburger ? null : (
+						<div className="flex h-full items-center justify-end px-6 sm:px-0 md:hidden">
+							<button
+								onClick={toggleHamburger}
+								className="hamburger-btn effect-shine"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									strokeWidth="1.5"
+									stroke="currentColor"
+									className="h-8 w-8"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+									/>
+								</svg>
+							</button>
+						</div>
+					)}
+					<HamburgerMenu
+						hamburgerState={showHamburger}
+						setHamburgerState={setShowHamburger}
+					/>
+				</div>
+				<div className="relative flex h-full w-full items-center justify-center">
+					<nav className="mynav black z-[100] flex h-full items-center justify-center w-full md:hidden">
+						<div
+							className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
+						></div>
+						<div
+							className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
+						></div>
+						<div
+							className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
+						></div>
+						<a
+							href="#gallery"
+							className="gallery-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
+							onClick={() => handleClick(0, "gallery")}
+						>
+							Gallery
+						</a>
+						<a
+							href="#price"
+							className="price-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
+							onClick={() => handleClick(1, "price")}
+						>
+							Price
+						</a>
+						<a
+							href="#info"
+							className="info-link header-item effect-shine z-10 inline-block w-1/3 cursor-pointer px-4 text-center"
+							onClick={() => handleClick(2, "info")}
+						>
+							Info
+						</a>
+					</nav>
+				</div>
+			</header>
 		</div>
-
 	)
 }
