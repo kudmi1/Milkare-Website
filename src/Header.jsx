f5f02cb9af3136176320b7f956ae0e203d4fec07
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-07-14 17:44:36 +0300

    fix header #3

--- src/Header.jsx
+++ src/Header.jsx
@@ -125,8 +125,8 @@ export default function Header({
 						setHamburgerState={setShowHamburger}
 					/>
 				</div>
-				<div className="relative flex h-full w-full items-center justify-center">
-					<nav className="mynav black z-[20] flex h-full items-center justify-center w-full md:hidden">
+				<div className="relative flex md:hidden h-full w-full items-center justify-center">
+					<nav className="mynav black z-[20] flex h-full items-center justify-center w-full ">
 						<div
 							className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
 						></div>
