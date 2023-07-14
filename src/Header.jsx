b29a92349475e416e391f1f2c1476dc4f61bc1be
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-07-14 17:39:17 +0300

    fix header#2

--- src/Header.jsx
+++ src/Header.jsx
@@ -126,7 +126,7 @@ export default function Header({
 					/>
 				</div>
 				<div className="relative flex h-full w-full items-center justify-center">
-					<nav className="mynav black z-[100] flex h-full items-center justify-center w-full md:hidden">
+					<nav className="mynav black z-[20] flex h-full items-center justify-center w-full md:hidden">
 						<div
 							className={`underline-animation pointer-events-none absolute bottom-0 left-0 z-0 block h-1 w-1/3 rounded-full transition-transform duration-200`}
 						></div>
