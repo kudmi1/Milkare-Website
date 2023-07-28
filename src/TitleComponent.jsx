ecfb81c72c48ae09561f1f53c0941bf97014da2f
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-07-28 16:45:33 +0300

    x

--- src/TitleComponent.jsx
+++ src/TitleComponent.jsx
@@ -3,7 +3,7 @@ export default function TitleComponent({ title }) {
 	return (
 		<div className='flex w-full items-center justify-center'>
 			<div
-				className={`relative flex h-16 items-center justify-center rounded-2xl border border-[#3b3b3b] bg-gradient-to-r from-[#212121d2] via-[#3f3f3fd2] to-[#212121d2] bg-cover px-4 backdrop-blur-[40px] min-w-[75%] md:min-w-[30rem] lg:h-16 mb-12 md:mb-24`}
+				className={`relative flex h-12 md:h-16 items-center justify-center rounded-2xl border border-[#3b3b3b] bg-gradient-to-r from-[#212121d2] via-[#3f3f3fd2] to-[#212121d2] bg-cover px-4 backdrop-blur-[40px] min-w-[75%] md:min-w-[30rem] lg:h-16 mb-12 md:mb-24`}
 			>
 				<img
 					src='icons/insta.svg'
