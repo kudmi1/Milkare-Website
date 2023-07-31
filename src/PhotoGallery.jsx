9eee12efd446f7fa736c1b44d0ba9c4a901fe687
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-07-31 12:07:50 +0300

    pinch

--- src/PhotoGallery.jsx
+++ src/PhotoGallery.jsx
@@ -3,6 +3,7 @@ import { Gallery, Item } from 'react-photoswipe-gallery'
 import { useInView } from 'react-intersection-observer'
 import { useState, useEffect, useRef } from 'react'
 import TitleComponent from './TitleComponent'
+import PhotoSwipeLightbox from 'photoswipe/dist/types/lightbox/lightbox'
 
 const code_names = [
 	'lucy',
@@ -51,7 +52,10 @@ const dimensions = [
 
 const options = {
 	zoom: false,
+	pinchToClose: true
 }
+
+
 
 export default function PhotoGallery({ language, content }) {
 	const [showGallery, setShowGallery] = useState(false)
@@ -90,6 +94,80 @@ export default function PhotoGallery({ language, content }) {
 		showGalleryWithDelay()
 	}
 
+	const uiElements = [
+    {
+      name: 'bulletsIndicator',
+      order: 9,
+      isButton: false,
+      appendTo: 'wrapper',
+      onInit: (el, pswpInstance) => {
+        let prevIndex = -1
+        const thumbnails = []
+
+        /* eslint-disable no-param-reassign */
+        el.style.position = 'absolute'
+        el.style.bottom = '20px'
+        el.style.left = '10px'
+        el.style.right = '0'
+        el.style.display = 'grid'
+        el.style.gridGap = '10px'
+        el.style.gridTemplateColumns = 'repeat(auto-fit, 40px)'
+        el.style.gridTemplateRows = 'repeat(auto-fit, 40px)'
+        el.style.justifyContent = 'center'
+        /* eslint-enable no-param-reassign */
+
+        const dataSource = pswpInstance.options.dataSource 
+
+        for (let i = 0; i < dataSource.length; i++) {
+          const slideData = dataSource[i]
+
+          const thumbnail = document.createElement('div')
+          thumbnail.style.transition = 'transform 0.15s ease-in'
+          thumbnail.style.opacity = '0.6'
+          thumbnail.style.cursor = 'pointer'
+          thumbnail.onclick = (e) => {
+            const target = e.target
+            const thumbnailEl =
+              target.tagName === 'IMG'
+                ? target.parentElement
+                : (e.target )
+            if (thumbnailEl) {
+              pswpInstance.goTo(thumbnails.indexOf(thumbnailEl))
+            }
+          }
+
+          const thumbnailImage = document.createElement('img')
+          thumbnailImage.setAttribute('src', slideData.msrc || '')
+          thumbnailImage.style.width = '100%'
+          thumbnailImage.style.height = '100%'
+          thumbnailImage.style.objectFit = 'cover'
+
+          thumbnail.appendChild(thumbnailImage)
+
+          el.appendChild(thumbnail)
+
+          thumbnails.push(thumbnail)
+        }
+
+        pswpInstance.on('change', () => {
+          if (prevIndex >= 0) {
+            const prevThumbnail = thumbnails[prevIndex]
+            prevThumbnail.style.opacity = '0.6'
+            prevThumbnail.style.cursor = 'pointer'
+            prevThumbnail.style.transform = 'scale(1)'
+          }
+
+          const currentThumbnail = thumbnails[pswpInstance.currIndex]
+          currentThumbnail.style.opacity = '1'
+          currentThumbnail.style.cursor = 'unset'
+          currentThumbnail.style.transform = 'scale(1.2)'
+
+          prevIndex = pswpInstance.currIndex
+        })
+      },
+    },
+  ]
+
 	return (
 		<div ref={ref} className='motion-reduce:transition-none'>
 			{inView ? (
@@ -104,7 +182,7 @@ export default function PhotoGallery({ language, content }) {
 						} transition-opacity duration-500`}
 						ref={gridRef}
 					>
-						<Gallery options={options}>
+						<Gallery options={options} uiElements={uiElements}>
 							{images.map((image, index) => (
 								<div key={index}>
 									<div
