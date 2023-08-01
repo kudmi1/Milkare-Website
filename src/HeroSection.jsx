f016f9c08376f6f9a034fc17b82d11ca20db8cb9
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-08-01 15:58:21 +0300

    vxc

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -12,21 +12,21 @@ export default function HeroSection() {
 			ref={ref}
 			className={`hero-section relative flex h-[50vh] max-h-[1080px] min-h-[600px] w-full flex-col items-center justify-center  md:h-[75vh] lg:h-screen`}
 		>
-			<div className={`big-banner w-full h-full bg-[url('/images/aqua.webp')] transition-opacity 
+			{/* <div className={`big-banner w-full h-full bg-[url('/images/aqua.webp')] transition-opacity 
 			${inView ? 'opacity-100' : 'opacity-0'}  
 			duration-500`}>
 
-			</div>
-			{/* <img
+			</div> */}
+			<img
 				src='./images/aqua.jpg'
 				srcSet='./images-accordion/aqua.webp 768w, ./images/aqua.jpg 800w'
-				// sizes={`
-    		// 	(max-width: 768px) 600px,
-    		// 	800px
-  			// `}
+				sizes={`
+    			(max-width: 768px) 600px,
+    			800px
+  			`}
 				className='hero-image w-full object-cover h-full '
 				alt='aqua_banner'
-			/> */}
+			/>
 			<Profile />
 		</section>
 	)
