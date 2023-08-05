7a4bf55c9a847f13d0b5c42b4b95a3adb7e920c8
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-08-05 14:54:47 +0300

    dasdcas

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -10,21 +10,26 @@ export default function HeroSection() {
 	return (
 		<section
 			ref={ref}
-			className={`hero-section relative flex h-[50vh] max-h-[1080px] min-h-[600px] w-full flex-col items-center justify-center  md:h-[75vh] lg:h-screen`}
+			className={`hero-section relative flex h-[50vh] max-h-[1080px] min-h-[600px] w-full flex-col items-center justify-center  sm:h-[75vh] lg:h-screen`}
 		>
-			<img
-				src='./images/aqua-hero.webp'
-				srcSet='./images/aqua-hero-mobile.webp 450w, ./images/aqua-hero-tablet.webp 1024w, ./images/aqua-hero.webp 1080w'
-				sizes={`
-    			(max-width: 600px) 450px,
-					(max-width: 1024px) 1024,
-    			1080px
-  			`}
-				className={`'hero-image w-full object-cover h-full ${inView ? 'opacity-100' : 'opacity-0'} transition-opacity 
-				${inView ? 'opacity-100' : 'opacity-0'}  
-				duration-500`}
+			<picture
+			className='w-full h-full'
+				// src='./images/aqua-hero.webp'
+				// srcSet='./images/aqua-hero-mobile.webp 800w, ./images/aqua-hero-tablet.webp 1024w, ./images/aqua-hero.webp 1920w'
+				// sizes={`
+    		// 	(max-width: 800px) 800px,
+				// 	(max-width: 1024px) 1024,
+    		// 	1920px
+  			// `}
 				alt='aqua_banner'
-			/>
+			>
+				<source media='(max-width: 500px)' srcSet='./images/aqua-hero-mobile.webp'/>
+				<source media='(max-width: 1024px)' srcSet='./images/aqua-hero-tablet.webp'/>
+				<img src="./images/aqua-hero.webp" type="image/webp" className={`'hero-image w-full object-cover h-full ${inView ? 'opacity-100' : 'opacity-0'} transition-opacity 
+				${inView ? 'opacity-100' : 'opacity-0'}  
+				duration-500`}/>
+			</picture>
+			
 			<Profile />
 		</section>
 	)
