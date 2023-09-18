7a661744384f72bce3a177055ef647132dd4d8b4
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-09-18 15:34:17 +0300

    fix hero image

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -12,21 +12,33 @@ export default function HeroSection() {
 		<div className='flex flex-col'>
 			<section
 				ref={ref}
-				className={`hero-section relative w-full h-[620px] lg:h-[580px]`}
+				className={`hero-section relative h-[620px] w-full lg:h-[580px]`}
 			>
-				<div className='w-full h-full'>
-					<img
+				<div className='h-full w-full'>
+					{/* <img
 						src='./images/aqua-hero.jpg'
 						alt=''
 						className={`h-full w-full object-cover transition-opacity 
 					${inView ? 'opacity-100' : 'opacity-0'} duration-500`}
 						srcSet='
-					./images/milka.webp 640w,
+					./images/milka.webp 640w 3x,
 					./images/aqua-hero-tablet.webp 1024w,
 					./images/aqua-hero.jpg 1920w
 					'
-					/>
-					{/* <TestProfile /> */}
+					/> */}
+					<picture>
+						<source srcSet='./images/milka.webp' media='(max-width: 640px)' />
+						<source
+							srcSet='./images/aqua-hero-tablet.webp'
+							media='(max-width: 1024)'
+						/>
+						<img
+							src='./images/aqua-hero.jpg'
+							alt='hero-image'
+							className={`h-full w-full object-cover transition-opacity 
+							${inView ? 'opacity-100' : 'opacity-0'} duration-500`}
+						/>
+					</picture>
 				</div>
 			</section>
 			<Profile />
