f6d77b02cbae037719bd1d7de6291a148b1635e1
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-09-19 20:53:28 +0300

    fix text

--- src/Pages/HomeSection.jsx
+++ src/Pages/HomeSection.jsx
@@ -12,28 +12,28 @@ export default function SectionHome() {
 		<div className='flex flex-col'>
 			<section
 				ref={ref}
-				className={`hero-section relative h-[80dvh] w-full lg:h-[100dvh]`}
+				className={`hero-section relative h-[646px] w-full `}
 			>
-				<div className='relative h-full w-full'>
+				<div className='relative h-full w-full '>
 					<picture>
 						<source srcSet='./images/milka.webp' media='(max-width: 640px)' />
 						<source srcSet='./images/milka.webp' media='(max-width: 1024)' />
 						<img
-							src='./images/milka.webp'
+							src='./images/milka.jpg'
 							alt='hero-image'
 							className={`h-full w-full object-cover object-center transition-opacity 
 							${inView ? 'opacity-100' : 'opacity-0'} duration-500`}
 						/>
 					</picture>
-					<h1 className='hero-text absolute hidden sm:block sm:bottom-[45%] lg:bottom-[40%] sm:left-6 lg:left-12'>
+					<h1 className='hero-text absolute hidden sm:left-6 sm:block sm:bottom-36 lg:bottom-1/2 lg:translate-y-1/2 lg:left-12'>
 						Commission <br /> Open
 					</h1>
-					<h1 className='hero-text absolute sm:hidden bottom-[77%] flex w-full justify-center '>
+					<h1 className='hero-text absolute bottom-0 py-6 flex w-full justify-center sm:hidden bg-gradient-to-r from-transparent via-mainGray to-transparent'>
 						Commission Open
 					</h1>
 				</div>
-				<Profile />
 			</section>
+			<Profile />
 			<SectionGallery />
 		</div>
 	)
