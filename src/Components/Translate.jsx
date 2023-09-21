92950bfdad499240bb25b9c53a383ac92e755dab
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-09-21 19:44:37 +0300

    vsdf

--- src/Components/Translate.jsx
+++ src/Components/Translate.jsx
@@ -6,10 +6,10 @@ export default function Translate() {
 	return (
 		<div className='language-block flex h-full w-full items-center justify-between lg:justify-end'>
 			<button
-				className={`language-item language-eng mr-4 h-14 w-1/2 text-xl lg:w-14 lg:border-none ${
+				className={`language-item language-eng mr-4 h-12 w-1/2 text-xl rounded-lg lg:w-14 lg:border-none lg:rounded-none ${
 					language === 'english'
 						? 'border border-thinLine bg-[#4b4b4b] bg-opacity-40 text-secondaryText'
-						: 'effect-shine text-mainText '
+						: 'effect-shine text-mainText border border-thinLine'
 				}`}
 				onClick={() => setLanguage('english')}
 			>
@@ -18,10 +18,10 @@ export default function Translate() {
 				</p>
 			</button>
 			<button
-				className={`language-item language-rus h-14 w-1/2 text-xl lg:w-14 lg:border-none ${
+				className={`language-item language-rus h-12 w-1/2 text-xl rounded-lg lg:w-14 lg:border-none lg:rounded-none ${
 					language === 'russian'
 						? 'border border-thinLine bg-[#4b4b4b] bg-opacity-40 text-secondaryText'
-						: 'effect-shine text-mainText '
+						: 'effect-shine text-mainText border border-thinLine'
 				} `}
 				onClick={() => setLanguage('russian')}
 			>
