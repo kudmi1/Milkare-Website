aef1306231ccdbf0e60ceba2a0061f9e16b122a6
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-09-21 18:11:14 +0300

    das

--- src/Components/Translate.jsx
+++ src/Components/Translate.jsx
@@ -4,29 +4,29 @@ import { useLanguageContext } from '../Providers/LanguageContext'
 export default function Translate() {
 	const { language, setLanguage } = useLanguageContext()
 	return (
-		<div className='language-block h-full flex w-full items-center justify-between lg:justify-end'>
+		<div className='language-block flex h-full w-full items-center justify-between lg:justify-end'>
 			<button
 				className={`language-item language-eng mr-4 h-14 w-1/2 rounded-lg text-xl lg:w-14 lg:border-none ${
-					language === 'english' ? 'text-secondaryText border border-thinLine lg:bg-[#161621] lg:bg-opacity-20 bg-[#161621] bg-opacity-40' : 'text-mainText effect-shine bg-[#161621] bg-opacity-20'
+					language === 'english'
+						? 'border border-thinLine bg-[#161621] bg-opacity-40 text-secondaryText lg:bg-[#161621] lg:bg-opacity-20'
+						: 'effect-shine bg-[#161621] bg-opacity-20 text-mainText lg:bg-opacity-0'
 				}`}
 				onClick={() => setLanguage('english')}
 			>
 				<p>
-					<span>
-						En
-					</span>
+					<span>En</span>
 				</p>
 			</button>
 			<button
 				className={`language-item language-rus h-14 w-1/2 rounded-lg text-xl lg:w-14 lg:border-none ${
-					language === 'russian' ? 'text-secondaryText border border-thinLine lg:bg-[#161621] lg:bg-opacity-20 bg-[#161621] bg-opacity-40' : 'text-mainText effect-shine bg-[#161621] bg-opacity-20'
+					language === 'russian'
+						? 'border border-thinLine bg-[#161621] bg-opacity-40 text-secondaryText lg:bg-[#161621] lg:bg-opacity-20'
+						: 'effect-shine bg-[#161621] bg-opacity-20 text-mainText lg:bg-opacity-0'
 				} `}
 				onClick={() => setLanguage('russian')}
 			>
 				<p>
-					<span >
-						Ru
-					</span>
+					<span>Ru</span>
 				</p>
 			</button>
 		</div>
