67e2c93087a54f73c59b9d246d0fd17f92b221fe
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-09-25 18:07:38 +0300

    width fix

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -5,7 +5,7 @@ import SpeedPaint from './SpeedPaint'
 export default function Gallery({ id }) {
 	return (
 		<section
-			className='section-entry mb-12 mt-6 scroll-m-12 lg:mb-0 lg:mt-0 lg:scroll-m-0'
+			className='section-entry mb-12 mt-6 scroll-m-12 lg:mb-0 lg:mt-0 lg:scroll-m-0 w-full'
 			id={id}
 		>
 			<Accordion />
