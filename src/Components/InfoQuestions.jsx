10f8f8531fa4f82a0f24c3a8bcb37b1eb3878daa
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-10-04 12:23:27 +0300

    accordion text

--- src/Components/InfoQuestions.jsx
+++ src/Components/InfoQuestions.jsx
@@ -76,12 +76,12 @@ export default function InfoQuestions() {
 		triggerOnce: true,
 	})
 	return (
-		<div ref={ref} className='mb-8'>
+		<div ref={ref} className='mb-8 w-full'>
 			{infoQuestion.map((text, index) => (
 				<div
 					onClick={() => handleOpen(index)}
 					key={index}
-					className={`info-panel active:bg-[#644f9c60] my-2 w-full cursor-pointer rounded-md border border-b-4 border-thinLine p-4 hover:border-b-secondaryText ${
+					className={`info-panel active:bg-[#644f9c60] my-2 w-full cursor-pointer rounded-md border border-b-4 p-4 border-thinLine  hover:border-b-secondaryText ${
 						isOpen === index ? ' border-b-secondaryText' : 'border-b-white/50'
 					} ${
 						inView ? 'opacity-100' : 'opacity-0'
@@ -96,8 +96,11 @@ export default function InfoQuestions() {
 							<img src='svg/eye-open.svg' alt='chevron' />
 						)}
 					</div>
+					<div className={`${isOpen === index ? 'max-h-96' : 'max-h-0 opacity-0 overflow-hidden'} transition-all duration-300`}>
+						<p className='mt-6'>{infoContent[index]}</p>
+					</div>
 
-					<div
+					{/* <div
 						className={`grid ${
 							isOpen === index ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'
 						} duration-500 transition-[grid-template-rows]`}
@@ -105,7 +108,7 @@ export default function InfoQuestions() {
 						<div className='overflow-hidden '>
 							<p className='mt-6 select-none '>{infoContent[index]}</p>
 						</div>
-					</div>
+					</div> */}
 				</div>
 			))}
 		</div>
