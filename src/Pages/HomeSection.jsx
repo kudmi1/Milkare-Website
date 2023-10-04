824acb2df292da4c4750775dfab4c73fe5974535
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-10-04 16:21:17 +0300

    sacdfvas

--- src/Pages/HomeSection.jsx
+++ src/Pages/HomeSection.jsx
@@ -3,15 +3,10 @@ import { useInView } from 'react-intersection-observer'
 import Gallery from '../Components/Gallery'
 
 export default function SectionHome() {
-	const { ref, inView } = useInView({
-		threshold: 0.2,
-		triggerOnce: true,
-	})
-
 	return (
 			<div className='flex flex-col items-center'>
 				<section
-					ref={ref}
+					
 					// className={`hero-section relative w-full md:h-[646px]`}
 					className={`hero-section relative h-[20vh] max-h-[1440px] min-h-[646px] w-full md:h-screen `}
 				>
@@ -28,8 +23,7 @@ export default function SectionHome() {
 							<img
 								src='./images/milka-wide.webp'
 								alt='hero-image'
-								className={`h-full w-full rounded-b-lg object-cover object-center transition-opacity 
-							${inView ? 'opacity-100' : 'opacity-0'} duration-500 `}
+								className={`h-full w-full rounded-b-lg object-cover object-center`}
 							/>
 						</picture>
 					</div>
