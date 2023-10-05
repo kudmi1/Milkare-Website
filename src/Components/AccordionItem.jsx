764afb7af3068d6bdd4737bea9f2e98890c8ed1a
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-10-05 14:13:02 +0300

    dascda

--- src/Components/AccordionItem.jsx
+++ src/Components/AccordionItem.jsx
@@ -7,22 +7,18 @@ export const AccordionItem = ({ faqItem, onClick, isOpen }) => {
 		<li
 			className={`'accordion-item my-2 w-full ${
 				isOpen ? 'border-b-secondaryText' : ''
-			} border-thinLine rounded-md border border-b-4 hover:border-b-secondaryText`}
+			} rounded-md border border-b-4 border-thinLine hover:border-b-secondaryText`}
 		>
 			<button
 				className='accordion-header subtitle-text flex w-full select-none items-center justify-between overflow-hidden p-4 text-lg lg:text-lg'
 				onClick={() => onClick()}
 			>
 				{faqItem.question}
-				<div>
-					
-				</div>
+				<div></div>
 				{isOpen ? (
-				<img src='svg/eye-close.svg' alt='chevron' />
-
-				): (
+					<img src='svg/eye-close.svg' alt='chevron' />
+				) : (
 					<img src='svg/eye-open.svg' alt='chevron' />
-
 				)}
 			</button>
 			<div
