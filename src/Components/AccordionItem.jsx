cd37f0d8411c9b0b5960159fd0d5a58487f05d43
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-10-05 13:59:15 +0300

    border fix

--- src/Components/AccordionItem.jsx
+++ src/Components/AccordionItem.jsx
@@ -1,22 +1,25 @@
 import React, { useRef } from 'react'
 
-export const AccordionItem = ({ faqItem, onClick, isOpen}) => {
+export const AccordionItem = ({ faqItem, onClick, isOpen }) => {
 	const itemRef = useRef(null)
-  
+
 	return (
-		<li className={`'accordion-item my-2 w-full rounded-md border border-b-4 border-thinLine hover:border-b-secondaryText`}>
+		<li
+			className={`'accordion-item my-2 w-full ${
+				isOpen ? 'border-b-secondaryText' : ''
+			} border-thinLine rounded-md border border-b-4 hover:border-b-secondaryText`}
+		>
 			<button
 				className='accordion-header subtitle-text flex w-full select-none items-center justify-between overflow-hidden p-4 text-lg lg:text-lg'
 				onClick={() => onClick()}
 			>
 				{faqItem.question}
-        <img src='svg/eye-open.svg' alt='chevron' />
+				<img src='svg/eye-open.svg' alt='chevron' />
 			</button>
 			<div
 				className={`accordion-collapse h-0 overflow-hidden transition-[height] duration-300`}
 				style={
 					isOpen ? { height: itemRef.current.scrollHeight } : { height: '0px' }
-
 				}
 			>
 				<div className='accordion-body p-4' ref={itemRef}>
