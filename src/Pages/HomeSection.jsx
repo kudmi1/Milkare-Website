e776bb022d52b204bcd7f2ede196af1df7711e6b
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-10-10 17:06:43 +0300

    fasdfasd

--- src/Pages/HomeSection.jsx
+++ src/Pages/HomeSection.jsx
@@ -6,33 +6,30 @@ export default function SectionHome() {
 	const [loaded, setLoaded] = useState(false)
 
 	useEffect(() => {
-    setLoaded(true)
-  }, [])
+		setLoaded(true)
+	}, [])
 	return (
-			<div className={`flex flex-col items-center transition-all duration-700`}>
-
-				<section
-					
-					// className={`hero-section relative w-full md:h-[646px]`}
-					className={`hero-section relative h-[70vh] max-h-[1440px] min-h-[646px] w-full lg:h-screen`}
-				>
-					<div className='relative h-full w-full'>
+		<div className={`flex flex-col items-center transition-all duration-700 `}>
+			<section
+				className={`hero-section relative h-[70vh] max-h-[1440px] min-h-[646px] w-full bg-[url(/images/milka.webp)] bg-cover bg-center bg-no-repeat lg:h-screen lg:bg-[url(/images/milka4x.webp)]`}
+			>
+				{/* <div className={`relative h-full w-full`}>
 						<picture>
 							<source
 								srcSet='./images/milka.webp'
 								media='(max-width: 1024px)'
 							/>
 							<img
-								src='./images/milka4x.webp'
+								src='./images/milka4x.webp3'
 								alt='hero-image'
 								// onContextMenu={false}
-								className={`h-full w-full rounded-b-lg object-cover ${loaded ? ' opacity-100 translate-y-0' : 'opacity-0 -translate-y-1'} transition-all duration-[.9s] object-center `}
+								className={`h-full w-full rounded-b-lg object-cover ${loaded ? ' opacity-100' : 'opacity-70 '} transition-all duration-[.9s] object-center `}
 							/>
 						</picture>
-					</div>
-					<Profile loaded={loaded} />
-				</section>
-				<Gallery />
-			</div>
+					</div> */}
+				<Profile loaded={loaded} />
+			</section>
+			<Gallery />
+		</div>
 	)
 }
