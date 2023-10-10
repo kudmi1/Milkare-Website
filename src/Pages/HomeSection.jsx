0f701e3f0f26b7ff8747ccbb00562acf99fda6b3
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-10-10 17:15:04 +0300

    bg

--- src/Pages/HomeSection.jsx
+++ src/Pages/HomeSection.jsx
@@ -11,22 +11,8 @@ export default function SectionHome() {
 	return (
 		<div className={`flex flex-col items-center transition-all duration-700 `}>
 			<section
-				className={`hero-section relative h-[70vh] max-h-[1440px] min-h-[646px] w-full bg-[url(/images/milka.webp)] bg-cover bg-center bg-no-repeat lg:h-screen  lg:bg-[url(/images/milka4x.webp)]`}
+				className={`hero-section relative h-[70vh] max-h-[1440px] min-h-[646px] w-full bg-[url(/images/milka.webp)] bg-cover bg-center bg-no-repeat lg:h-screen lg:bg-[url(/images/milka4x.webp)]`}
 			>
-				{/* <div className={`relative h-full w-full`}>
-						<picture>
-							<source
-								srcSet='./images/milka.webp'
-								media='(max-width: 1024px)'
-							/>
-							<img
-								src='./images/milka4x.webp3'
-								alt='hero-image'
-								// onContextMenu={false}
-								className={`h-full w-full rounded-b-lg object-cover ${loaded ? ' opacity-100' : 'opacity-70 '} transition-all duration-[.9s] object-center `}
-							/>
-						</picture>
-					</div> */}
 				<Profile loaded={loaded} />
 			</section>
 			<Gallery />
