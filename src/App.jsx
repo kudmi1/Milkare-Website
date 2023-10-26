a70fdb25ddfa166ff99d27ebe8fe66f996486ee2
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2023-10-26 15:46:36 +0300

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,22 @@ function App() {
 	const headerRef = useRef(null)
 	const sectionRef = useRef(null)
 
+	const loaderContainer = document.querySelector('.loader-container')
+	const loader = document.querySelector('.loader')
+
+	useEffect(() => {
+		window.addEventListener('load', () => {
+			setTimeout(() => {
+				loaderContainer.style.opacity = 0
+				loader.style.transform = 'scale(0.5)'
+				loaderContainer.addEventListener(
+					'transitionend',
+					() => (loaderContainer.style.display = 'none')
+				)
+			}, 300)
+		})
+	}, [])
+
 	useEffect(() => {
 		switch (location.pathname) {
 			case '/':
@@ -31,24 +47,24 @@ function App() {
 	}, [location])
 
 	return (
-			<div className='App relative flex min-h-screen flex-col bg-[url("/images/bg.webp")] md:bg-[url("/images/bg-transformed.webp")]'>
-				<header ref={headerRef}>
-					<Header page={page} setPage={setPage} />
-				</header>
-				<main ref={sectionRef}>
-					<Routes>
-						<Route path='/' element={<SectionHome />} />
-						<Route path='/price' element={<SectionPrice id={'price'} />} />
-						<Route path='/info' element={<SectionInfo id={'info'} />} />
-						<Route path='*' element={<NotFoundPage />} />
-					</Routes>
-					<ToTop />
-				</main>
-				<footer className='mt-auto'>
-					<Footer />
-				</footer>
-			</div>
-		)
+		<div className='App relative flex min-h-screen flex-col bg-[url("/images/bg.webp")] md:bg-[url("/images/bg-transformed.webp")]'>
+			<header ref={headerRef}>
+				<Header page={page} setPage={setPage} />
+			</header>
+			<main ref={sectionRef}>
+				<Routes>
+					<Route path='/' element={<SectionHome />} />
+					<Route path='/price' element={<SectionPrice id={'price'} />} />
+					<Route path='/info' element={<SectionInfo id={'info'} />} />
+					<Route path='*' element={<NotFoundPage />} />
+				</Routes>
+				<ToTop />
+			</main>
+			<footer className='mt-auto'>
+				<Footer />
+			</footer>
+		</div>
+	)
 }
 
 export default App
--- src/Pages/HomeSection.jsx
+++ src/Pages/HomeSection.jsx
@@ -1,8 +1,8 @@
 import Profile from '../Components/Profile'
 import Gallery from '../Components/Gallery'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-export default function SectionHome({ commissionStatus }) {
+export default function SectionHome() {
 	const [loaded, setLoaded] = useState(false)
 
 	useEffect(() => {
@@ -11,9 +11,15 @@ export default function SectionHome({ commissionStatus }) {
 	return (
 		<div className={`flex flex-col items-center transition-all duration-700 `}>
 			<section
-				className={`hero-section relative h-[70vh] max-h-[1440px] min-h-[646px] w-full bg-[url(/images/milka.webp)] bg-cover bg-center bg-no-repeat lg:h-screen lg:bg-[url(/images/milka4x.webp)]`}
+				className={`hero-section relative h-[70vh] max-h-[1440px] min-h-[646px] w-full bg-opacity-30 bg-[url(/images/milka.webp)] bg-cover bg-center bg-no-repeat lg:h-screen`}
 			>
-				<Profile loaded={loaded} commissionStatus={commissionStatus}/>
+				<img
+					src='/images/milka4x.webp'
+					alt='milka-banner-image'
+					className={`hidden h-full w-full object-cover lg:block`}
+					onContextMenu={false}
+				/>
+				<Profile loaded={loaded} />
 			</section>
 			<Gallery />
 		</div>
